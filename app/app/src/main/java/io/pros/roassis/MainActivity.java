4263b5d28d93afd2d4b9790dae7285cdf7510632
Ivan Megias Duran <ivan92.md@gmail.com>
2016-07-22 09:39:29 +0200

    Added button in map to call

--- app/app/src/main/java/io/pros/roassis/MainActivity.java
+++ app/app/src/main/java/io/pros/roassis/MainActivity.java
@@ -2,9 +2,12 @@ package io.pros.roassis;
 
 import android.app.NotificationManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -48,5 +51,6 @@ public class MainActivity extends AppCompatActivity {
         Intent myIntent = new Intent(this, MapsActivity.class);
         startActivity(myIntent);
     }
+
 
 }
--- app/app/src/main/java/io/pros/roassis/MapsActivity.java
+++ app/app/src/main/java/io/pros/roassis/MapsActivity.java
@@ -1,9 +1,14 @@
 package io.pros.roassis;
 
 import android.app.NotificationManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.games.snapshot.Snapshot;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -82,5 +87,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+    }
+
+    public void call(View view) {
+        Intent myIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "659984265"));
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(myIntent);
     }
 }
